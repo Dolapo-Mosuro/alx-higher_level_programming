85b92a9fb063d37b41292c735aed4667d1c4ef75
dolapo-mosuro <mosurodolapo@gmail.com>
2023-05-16 20:51:08 -0700

    task 3 update

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+def print_reversed_list_integer(my_list=[]):
+    list_length = len(my_list)
+    for i in range(list_length - 1, -1, -1):
+        print("{}".format(my_list[i]))
+
+# Test
+my_list = [10, 22, 30, 44, 50]
+print_reversed_list_integer(my_list)
