7516c928b7522766d702431a3c291d990b8b4850
dolapo-mosuro <mosurodolapo@gmail.com>
2023-05-17 22:06:36 -0700

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+from add_0 import add
+
+result = add(a, b)
+print('{} + {} = {}'.format(a, b, result))
+
+a = 10
+b = 5
+from add_0 import sub
+
+result = sub(a, b)
+print('{} + {} = {}'.format(a, b, result))
+
+a = 10
+b = 5
+from add_0 import mul
+
+result = mul(a, b)
+print('{} + {} = {}'.format(a, b, result))
+
+a = 10
+b = 5
+from add_0 import div
+
+result = div(a, b)
+print('{} + {} = {}'.format(a, b, result))
+
+a = 10
+b = 5
+from add_0 import add
+
+result = add(a, b)
+print('{} + {} = {}'.format(a, b, result))
