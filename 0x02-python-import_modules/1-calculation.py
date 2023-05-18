ba7bd4c1953b354036ef11b74b498c7cc4a7fcf0
dolapo-mosuro <mosurodolapo@gmail.com>
2023-05-17 22:10:41 -0700

    correction of import name

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
 a = 10
 b = 5
-from add_0 import add
+from calculator_1.py import add
 
 result = add(a, b)
 print('{} + {} = {}'.format(a, b, result))
 
 a = 10
 b = 5
-from add_0 import sub
+from calculator_1.py import sub
 
 result = sub(a, b)
 print('{} + {} = {}'.format(a, b, result))
 
 a = 10
 b = 5
-from add_0 import mul
+from calculator_1.py import mul
 
 result = mul(a, b)
 print('{} + {} = {}'.format(a, b, result))
 
 a = 10
 b = 5
-from add_0 import div
+from calculator_1.py import div
 
 result = div(a, b)
-print('{} + {} = {}'.format(a, b, result))
-
-a = 10
-b = 5
-from add_0 import add
-
-result = add(a, b)
 print('{} + {} = {}'.format(a, b, result))
