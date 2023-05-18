441661915e501cb7a1ecd25e0352f74bebd85b82
dolapo-mosuro <mosurodolapo@gmail.com>
2023-05-18 03:51:01 -0700

    indentation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ a = 1
 b = 2
 if __name__ == "__main__":
 from add_0 import add
-sum = add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+     sum_result = add(a, b)
+     print("{} + {} = {}".format(a, b (sum_result)))
