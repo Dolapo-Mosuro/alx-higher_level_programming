7e7b6f9cdd86c15869ff5ffffce8bd3fd313645a
Dolapo Mosuro <mosurodolapo@gmail.com>
2023-08-14 17:21:21 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ b = 2
 if __name__ == "__main__":
     from add_0 import add
     sum_result = add(a, b)
-    print("{} + {} = {}".format(a, b, sum_result))
+    print("{} + {} = {}".format(a, b,  sum_result))
