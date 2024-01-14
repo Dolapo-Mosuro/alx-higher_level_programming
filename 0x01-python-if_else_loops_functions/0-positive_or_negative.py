aea8d29575e57302c28a4641f197636fbac11b8a
Dolapo-mosuro <mosurodolapo@gmail.com>
2024-01-14 14:52:21 -0800

    corrections

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 import random
 
-#@number- Varible stores random numbers between -10 and 10
-
-#It outputs the number followed by 
-#the number "is positive" if the number is greater than 0
-#the number "is zero" if the number is equal to 0
-#the number "is negative" if the number is less than 0
-#follwed by new-line
-
 number = random.randint(-10, 10)
 
 if (number > 0):
-print(number, "is positive")
+    print(number, "is positive")
 elif (number == 0):
-print(number, "is zero")
+    print(number, "is zero")
 else:
-print(number, "is negative")
+    print(number, "is negative")
