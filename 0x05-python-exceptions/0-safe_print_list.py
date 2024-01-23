55db7da190a3a6e820c6ff49dd2457de00e920c4
Dolapo-mosuro <mosurodolapo@gmail.com>
2024-01-22 20:55:59 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,12 +7,10 @@ def safe_print_list(my_list=[], x=0):
 
     while i < x:
         try:
-            print(f"{my_list=[i]}", end="")
+            print(f"{my_list[i]}", end="")
             total += 1
         except IndexError:
             break
-
-        i += 1
 
         print()
         return total
