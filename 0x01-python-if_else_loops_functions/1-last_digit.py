37df4472a4fd91963d7ee9ba8fa489f659e4f7f2
dolapo-mosuro <mosurodolapo@gmail.com>
2024-04-29 10:59:46 +0100

    task 3

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,8 @@ number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
 
+if number < 0:
+    last_digit = -last_digit
 if last_digit == 0:
     out_put = "and is 0"
 elif last_digit > 5:
